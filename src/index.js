e18afc8a08c810f91f629978ce56adf735f565e7
AXLRSR <62110917+AXLRSR@users.noreply.github.com>
2020-06-23 17:55:12 +0200

    ✨ Add & remove movies

--- src/index.js
+++ src/index.js
@@ -4,30 +4,62 @@ import ReactDOM from 'react-dom';
 import "./styles.css";
 
 class App extends React.Component {
+
+  state = {
+    movies: [
+      {id: 1, title: "Avengers"},
+      {id: 2, title: "Avengers: Age of Ultron"},
+      {id: 3, title: "Avengers: Infinity War"},
+      {id: 4, title: "Avengers: Endgame"}
+    ],
+
+    newMovie: ""
+  };
+
+  handleDelete = (id) => {
+    const movies = [...this.state.movies];
+    const index = movies.findIndex((movie) =>  movie.id === id);
+
+    movies.splice(index, 1);
+
+    this.setState({movies});
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const id = new Date().getTime();
+    const title = this.state.newMovie;
+
+    const movies = [...this.state.movies];
+    movies.push({id, title});
+    
+    this.setState({movies, newMovie: ""})
+  };
+
+  handleChange = (event) => {
+    this.setState({newMovie: event.currentTarget.value});
+  }
+
   render() {
     return <div className="content">
       <div className="header">
         <h1 className="header__title">WatchList</h1>
-        
-        <form className="search">
-          <input className="search__bar" type="text" placeholder="Your movie" />
+
+        <form className="search" onSubmit={this.handleSubmit}>
+          <input className="search__bar" type="text" placeholder="Your movie" value={this.state.newMovie} onChange={this.handleChange} />
           <input className="search__add" type="submit" value="Add" />
         </form>
       </div>
 
       <ul className="list">
-        <li className="list__item">
-          <input className="list__check" type="checkbox" id="1" />
-          <label className="list__label" for="1">Avengers</label>
-        </li>
-        <li className="list__item">
-          <input className="list__check" type="checkbox" id="2" />
-          <label className="list__label" for="2">Avengers: Age of Ultron</label>
-        </li>
-        <li className="list__item">
-          <input className="list__check" type="checkbox" id="3" />
-          <label className="list__label" for="3">Avengers: Infinity War</label>
-        </li>
+        {this.state.movies.map(movie => (
+          <li className="list__item">
+            <input className="list__check" type="checkbox" id={movie.id} />
+            <label className="list__label" for={movie.id}>{movie.title}</label>
+            <button className="list__delete" onClick={() => this.handleDelete(movie.id)}>×</button>
+          </li>
+        ))}
       </ul>
     </div>
   }
